2dac614f975c0a91b5b7172efaef34be0c658e57
narayan-mindfire <narayanp@mindfiresolutions.com>
2025-03-13 13:10:48 +0530

    google signin problem

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -6,11 +6,32 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { FC, useCallback } from "react";
+import React, { FC, useCallback, useEffect } from "react";
 import { WelcomeParams } from "../TypesDefined/NavTypes";
 import { useFocusEffect, useTheme } from "@react-navigation/native";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
 const Welcome: FC<WelcomeParams> = ({ navigation }) => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        "691740424422-0lqh5fmsc9navf692oagkp5f3rs20hq5.apps.googleusercontent.com",
+    });
+  }, []);
+  const googleSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      console.log("has play service");
+      const userInfo = await GoogleSignin.signIn();
+      console.log("signin function called");
+      console.log("Google Sign-In Success:", userInfo);
+      Alert.alert("Success", `Welcome, ${userInfo.user.name}!`);
+    } catch (error) {
+      console.log("Google Sign-In Error:", error);
+      Alert.alert("Error", "Google Sign-In Failed.");
+    }
+  };
+
   useFocusEffect(
     useCallback(() => {
       const onBackPress = () => {
@@ -49,6 +70,14 @@ const Welcome: FC<WelcomeParams> = ({ navigation }) => {
       >
         <Text style={[styles.btntxt, { color: colors.text }]}>
           Authenticate
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.btn, { backgroundColor: colors.card }]}
+        onPress={() => googleSignIn()}
+      >
+        <Text style={[styles.btntxt, { color: colors.text }]}>
+          googleSignin
         </Text>
       </TouchableOpacity>
     </View>
